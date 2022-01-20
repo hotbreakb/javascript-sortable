02be9376f55b31916c19aceda0c2eb0601aa149d
hotbreakb <areyhappy12@gmail.com>
2022-01-20 14:08:29 +0900

    draggable addEventListener

--- build/sortable.js
+++ build/sortable.js
@@ -1,2 +1,8 @@
 "use strict";
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => { draggable.classList.add('dragging'); });
+    draggable.addEventListener('dragend', () => { draggable.classList.remove('dragging'); });
+});
 //# sourceMappingURL=sortable.js.map
