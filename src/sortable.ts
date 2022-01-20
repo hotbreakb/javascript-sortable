e145d6fba2c67335972451ef79bf9b5a62ff7f17
hotbreakb <areyhappy12@gmail.com>
2022-01-20 14:23:45 +0900

    containers addEventListener

--- src/sortable.ts
+++ src/sortable.ts
@@ -1,8 +1,22 @@
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
+// (2)
 draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => { draggable.classList.add('dragging') });
-    draggable.addEventListener('dragend', () => { draggable.classList.remove('dragging') });
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    });
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    });
 });
 
+// (3)
+containers.forEach(container => {
+    container.addEventListener('dragover', () => {
+        const draggable = document.querySelector('.dragging');
+
+        if (draggable !== null) container.appendChild(draggable);
+    });
+});
